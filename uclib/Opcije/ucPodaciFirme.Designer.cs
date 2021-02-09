9ceabe21a2ac14a1645827bbeda87b10f10045af
NikoTrif <60791847+NikoTrif@users.noreply.github.com>
2021-02-09 23:31:31 +0100

    TD 2.3.c - zapoceto uredjivanje

---
+++ uclib/Opcije/ucPodaciFirme.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace uclib.Opcije
+{
+    partial class ucPodaciFirme
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbLogo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbFirma = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbDelatnost = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbAdresa = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbTelefon = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbMail = new System.Windows.Forms.TextBox();
+            this.pbSetLogo = new System.Windows.Forms.PictureBox();
+            this.dBrowse = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSetLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Logo:";
+            // 
+            // tbLogo
+            // 
+            this.tbLogo.Location = new System.Drawing.Point(115, 42);
+            this.tbLogo.Name = "tbLogo";
+            this.tbLogo.Size = new System.Drawing.Size(237, 22);
+            this.tbLogo.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Naziv firme:";
+            // 
+            // tbFirma
+            // 
+            this.tbFirma.Location = new System.Drawing.Point(115, 70);
+            this.tbFirma.Name = "tbFirma";
+            this.tbFirma.Size = new System.Drawing.Size(237, 22);
+            this.tbFirma.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Delatnost:";
+            // 
+            // tbDelatnost
+            // 
+            this.tbDelatnost.Location = new System.Drawing.Point(115, 98);
+            this.tbDelatnost.Name = "tbDelatnost";
+            this.tbDelatnost.Size = new System.Drawing.Size(237, 22);
+            this.tbDelatnost.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Adresa:";
+            // 
+            // tbAdresa
+            // 
+            this.tbAdresa.Location = new System.Drawing.Point(115, 126);
+            this.tbAdresa.Name = "tbAdresa";
+            this.tbAdresa.Size = new System.Drawing.Size(237, 22);
+            this.tbAdresa.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 157);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Telefon:";
+            // 
+            // tbTelefon
+            // 
+            this.tbTelefon.Location = new System.Drawing.Point(115, 154);
+            this.tbTelefon.Name = "tbTelefon";
+            this.tbTelefon.Size = new System.Drawing.Size(237, 22);
+            this.tbTelefon.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 185);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 17);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "e-mail:";
+            // 
+            // tbMail
+            // 
+            this.tbMail.Location = new System.Drawing.Point(115, 182);
+            this.tbMail.Name = "tbMail";
+            this.tbMail.Size = new System.Drawing.Size(237, 22);
+            this.tbMail.TabIndex = 1;
+            // 
+            // pbSetLogo
+            // 
+            this.pbSetLogo.Location = new System.Drawing.Point(461, 42);
+            this.pbSetLogo.Name = "pbSetLogo";
+            this.pbSetLogo.Size = new System.Drawing.Size(162, 162);
+            this.pbSetLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbSetLogo.TabIndex = 2;
+            this.pbSetLogo.TabStop = false;
+            // 
+            // dBrowse
+            // 
+            this.dBrowse.Location = new System.Drawing.Point(358, 42);
+            this.dBrowse.Name = "dBrowse";
+            this.dBrowse.Size = new System.Drawing.Size(34, 23);
+            this.dBrowse.TabIndex = 3;
+            this.dBrowse.Text = "...";
+            this.dBrowse.UseVisualStyleBackColor = true;
+            // 
+            // ucPodaciFirme
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dBrowse);
+            this.Controls.Add(this.pbSetLogo);
+            this.Controls.Add(this.tbMail);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbTelefon);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbAdresa);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbDelatnost);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbFirma);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbLogo);
+            this.Controls.Add(this.label1);
+            this.Name = "ucPodaciFirme";
+            this.Size = new System.Drawing.Size(709, 594);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSetLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbLogo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbFirma;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbDelatnost;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbAdresa;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbTelefon;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbMail;
+        private System.Windows.Forms.PictureBox pbSetLogo;
+        private System.Windows.Forms.Button dBrowse;
+    }
+}

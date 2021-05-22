26c0060f54940875439adca542c59bacfb3b432b
NikoTrif <60791847+NikoTrif@users.noreply.github.com>
2021-05-22 15:30:40 +0200

    repNalogF - napravljen radni nalog za firme

---
+++ iflib/ReportClasses/clRadniNalogPos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iflib.ReportClasses
+{
+    public class clRadniNalogPos
+    {
+        public string BrojNaloga { get; set; }
+        public string IDFirme { get; set; }
+        public string KontaktOsoba { get; set; }
+        public string KontaktTelefon { get; set; }
+        public string NazivFirme { get; set; }
+        public string eMail { get; set; }
+        public string Datum { get; set; }
+        public string Uredjaj { get; set; }
+        public string Proizvodjac { get; set; }
+        public string Model { get; set; }
+        public string SerijskiBroj { get; set; }
+        public string Pribor { get; set; }
+        public string OpisKvara { get; set; }
+        public string Izvestaj { get; set; }
+    }
+}

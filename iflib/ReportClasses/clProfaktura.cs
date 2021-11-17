c6fdef354d4ee09b849312005d57bb405f43040b
NikoTrif <60791847+NikoTrif@users.noreply.github.com>
2021-11-17 19:03:44 +0100

    TD 3.5 - nastavljeno

---
+++ iflib/ReportClasses/clProfaktura.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iflib.ReportClasses
+{
+    public class clProfaktura
+    {
+        public int BrojProfakture { get; set; }
+        public string Datum { get; set; }
+        public string Klauzula { get; set; }
+        public string IDKlijenta { get; set; }
+        public string Klijent { get; set; }
+        public string Adresa { get; set; }
+        public string Grad { get; set; }
+        public string PIB { get; set; }
+        public string MatBr { get; set; }
+        public float Total { get; set; }
+    }
+
+    public class clRoba
+    {
+        public string Opis { get; set; }
+        public float Kolicina { get; set; }
+        public string JedinicaMere { get; set; }
+        public float Cena { get; set; }
+        public float PDV { get; set; }
+        public float UkupnaCena { get; set; }
+    }
+}
